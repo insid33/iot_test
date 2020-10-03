a3854859271fb147842bc8f715682e5e800dce5e
beta2lab <contatobetalab@gmail.com>
2020-10-03 14:35:10 -0300

    wifi setup done

---
+++ setup_wifi.py
@@ -0,0 +1,31 @@
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+import network
+from machine import Pin
+import dht
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'SSID'
+password = 'PASSWORD'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+sensor = dht.DHT22(Pin(4))
